25876d0703eadafa4bfe51b7bab5c47eed35aab7
Christian Horler <csh.sammt@gmail.com>
2025-09-04 14:35:37 +0200

    More JS Learning + Moved Posters

--- server/app.js
+++ server/app.js
@@ -3,13 +3,14 @@ const path = require("node:path");
 
 const app = express();
 const port = 3000;
+const siteRouter = require('./routes/site');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res) =>
-    res.render('home', {title: 'Welcome'}))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', siteRouter);
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
